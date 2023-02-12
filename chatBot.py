42e6a4341778b052a527b07239ae0bbc504a498a
AlexeyPo2867 <113497026+AlexeyPo2867@users.noreply.github.com>
2023-02-12 13:50:48 +0300

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -5,7 +5,7 @@ from bot_commands import *
 # moscow_my_Bot - Бот в Телеграме
 # myBot - username
 
-app = Updater("5881762588:AAHo_i9FeeCb-qvee3GQbpruXsqG6P1mmcc")
+app = Updater("")
 
 # hello - имя команды в Телеграмм (запускается - /hello, /time, /help/ /sum)
 # hello_command, time_command, help_command, sum_command - функции которые вызываются пр наборе 
